e8f407c0889b68fbc2475828fb0e8e8543ff3cb2
Peyal Hasan <peyalhasan143@gmail.com>
2025-11-13 16:33:01 +0600

    Favorite icon

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchTask from './SearchTask';
 import TaskActions from './TaskActions';
 import TaskList from './TaskList';
 
 const TaskBoard = () => {
+    const defaultTask = {
+        'id': crypto.randomUUID(),
+        'title': 'Learn React Native',
+        "description": "I want to Learn React such than I can treat it like my slave and make it do whatever I want to do.",
+        "tags": ["web", "react", "js"],
+        "priority": "High", 
+        "isFavorite": true
+    }
+    const [tasks, setTasks] = useState([defaultTask]);
     return (
         <div>
             <section className="mb-20 " id="tasks">
@@ -15,8 +24,8 @@ const TaskBoard = () => {
                     <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
 
                         <TaskActions></TaskActions>
-                        <TaskList></TaskList>
-                        
+                        <TaskList tasks={tasks} ></TaskList>
+
                     </div>
                 </div>
             </section>
