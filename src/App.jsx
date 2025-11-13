df96944be4d8bd26506a386308d1b7eebfcd64e0
Peyal Hasan <peyalhasan143@gmail.com>
2025-11-13 15:47:26 +0600

    Complete Design part

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import Hero from './Components/Hero';
+import TaskBoard from './Task/TaskBoard';
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <Footer></Footer>
+      <div className='flex flex-col justify-center items-center' >
       <Hero></Hero>
+      <TaskBoard></TaskBoard>
+      </div>
+      <Footer></Footer>
 
     </div>
   );
---
+++ src/Task/TaskBoard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import SearchTask from './SearchTask';
+import TaskActions from './TaskActions';
+import TaskList from './TaskList';
+
+const TaskBoard = () => {
+    return (
+        <div>
+            <section className="mb-20 " id="tasks">
+
+                <div className="container">
+                    <div className="p-2 flex justify-end">
+                        <SearchTask></SearchTask>
+                    </div>
+                    <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+
+                        <TaskActions></TaskActions>
+                        <TaskList></TaskList>
+                        
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default TaskBoard;
