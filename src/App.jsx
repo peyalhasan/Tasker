ee4e6f39388cb932071419c3fd08615c41fc96cb
Peyal Hasan <peyalhasan143@gmail.com>
2025-11-13 14:59:57 +0600

    Tasker

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Hero from './Components/Hero';
+const App = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Footer></Footer>
+      <Hero></Hero>
+
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="py-6 md:py-8 fixed top-0 w-full bg-[#191D26] z-50">
+                <div className="container mx-auto flex items-center justify-between gap-x-6">
+                    <a href="/">
+                        <img className="h-[45px]" src={'/Tasker.png'} alt="Tasker" />
+                    </a>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
